303b86a363931491891be4430898c9b18062b1a7
Janice Chan <janice.chan1288@gmail.com>
2022-05-19 16:29:41 -0400

    set up component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import Search from "./Search"
+import Form from './Form'
+
+const Header = () => {
+    return (
+        <div>
+            <p>Header Logo</p>
+            <Search />
+            <Form />
+        </div>
+    )
+}
+
+export default Header
