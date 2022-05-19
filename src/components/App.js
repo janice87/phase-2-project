42d18328b64a50a98b10727b3a9646754f1bdf4b
Janice Chan <janice.chan1288@gmail.com>
2022-05-19 16:29:09 -0400

    update App.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import NavBar from './NavBar';
+import PageContainer from './PageContainer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <NavBar />
+      <PageContainer />
+    </div>
+  );
+}
+
+export default App;
