bc8924bbf4b3278e4051d3e5cce2bc81a6552808
Janice Chan <janice.chan1288@gmail.com>
2022-05-20 15:04:41 -0400

    update route

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ActivityCard from "./ActivityCard"
 
-function ActivityList ({activities}) {
+const ActivityList = ({activities}) => {
   
     const activityArray = activities.map(activity => (  
         <ActivityCard key={activity.id} activity={activity} />
-        //console.log(activity)
-    ))
+     ))
 
     return (
         <div>
