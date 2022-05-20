7432130a1a09af6adfd38facd4ee0621ca3d4e4f
Janice Chan <janice.chan1288@gmail.com>
2022-05-20 16:29:58 -0400

    add cb function and filter for search

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,22 +1,30 @@
 import React, {useState, useEffect} from 'react'
-import Form from './Form'
+//import Form from './Form'
 import Search from './Search'
 import ActivityList from './ActivityList'
 
 const PageContainer = () => {
 const [activities, setActivities] = useState([])
+const [searchTerm, setSearchTerm] = useState("")
 
 useEffect (() => {
         fetch('http://localhost:4000/all')
         .then(res => res.json())
         .then(data => setActivities(data))
       }, [])
+
+const handleSearchQuery = (searchQuery) => {
+    setSearchTerm(searchQuery)
+}
+
+const searchedActivities = activities.filter(activity => 
+    activity.name.toLowerCase().includes(searchTerm.toLowerCase()))
     
     return (
       <div>
         {/* <Form /> */}
-        <Search />
-        <ActivityList activities={activities} />
+        <Search searchTerm={searchTerm} onHandleSearch={handleSearchQuery} />
+        <ActivityList activities={searchedActivities} />
       </div>
 
     )   
