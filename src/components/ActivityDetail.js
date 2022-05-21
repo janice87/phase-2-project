fd46a4ae76c18a426ec744d208842b14ab59c8d9
Janice Chan <janice.chan1288@gmail.com>
2022-05-21 15:40:04 -0400

    add elements to render

--- src/components/ActivityDetail.js
+++ src/components/ActivityDetail.js
@@ -2,13 +2,13 @@ import React, {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
 
 const ActivityDetail = () => {
-    const [activity, setActivity] = useState(null)
+    const [activity, setActivity] = useState({})
     const {id} = useParams(); 
        
     useEffect(() => {
         fetch(`http://localhost:4000/all/${id}`)
         .then(res => res.json())
-        .then(activityObj => console.log(activityObj))
+        .then(activityObj => setActivity(activityObj))
     }, [id])
     
     const {name, image, type, address, island, description} = activity
@@ -17,6 +17,11 @@ const ActivityDetail = () => {
         <div>
            <h1>I'm from activity detail</h1>
            <p>{name}</p>
+           <p>{image}</p>
+           <p>{type}</p>
+           <p>{address}</p>
+           <p>{island}</p>
+           <p>{description}</p>
         </div>
     )
 }
