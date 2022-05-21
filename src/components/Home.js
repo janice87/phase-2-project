87f8c515c7888d43c7de686000a1dc15ac4b80dc
Janice Chan <janice.chan1288@gmail.com>
2022-05-20 21:44:40 -0400

    add headers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+// import { Typography } from '@mui/material';
+
 const Home = () => {
     return (
-        <div>
-           <h3>Hawaii Eats</h3>
+        <div className="home">
+           <CssBaseline />
+          <Container maxWidth="xl">   
+          <h1>aloha</h1>         
+             <br />  
+            <h3>your adventure awaits ...</h3>
+         </Container>
         </div>
     ) 
 }
 
 export default Home
+
+{/* <Typography variant="h2" align='center'>
+Aloha
+</Typography> */}
