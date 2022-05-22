b273cb81457040be5f65c27b641d0a1afd516f9a
Janice Chan <janice.chan1288@gmail.com>
2022-05-21 20:49:04 -0400

    pass in props

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ActivityCard from "./ActivityCard"
 
-const ActivityList = ({activities}) => {
+const ActivityList = ({activities, onHandleDeleteCard, onAddToTrip }) => {
   
     const activityArray = activities.map(activity => (  
-        <ActivityCard key={activity.id} activity={activity} />
+        <ActivityCard key={activity.id} activity={activity} onHandleDeleteCard={onHandleDeleteCard} onAddToTrip={onAddToTrip}/>
      ))
 
     return (
