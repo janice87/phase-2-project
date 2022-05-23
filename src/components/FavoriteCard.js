71ecabb4a456057089d8b2a28653bb86af2b180c
Janice Chan <janice.chan1288@gmail.com>
2022-05-23 13:55:55 -0400

    update favorite cards

--- src/components/FavoriteCard.js
+++ src/components/FavoriteCard.js
@@ -1,11 +1,26 @@
-const FavoriteCard = ({trip}) => {
-const {name, address} = trip
-    return (
-        <div>
-            <h1>hello from card</h1>
-            <h1>{name}</h1>
-            <h1>{address}</h1>
+import Button from '@mui/material/Button';
+
+const FavoriteCard = ({fav, onHandleDeleteFav}) => {
+const {name, image, address, id} = fav
+
+    const handleRemoveFav = () => {
+        fetch(`http://localhost:4000/favorites/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(() => onHandleDeleteFav(id))
+    }
+
+    return (                      
+        <div className="fav-card">
+            <h3>{name}</h3>
+            <div className="image">          
+            <img src={image} alt={name} />
+            </div>
+            <p>📍{address}</p>
+            <Button variant="outlined" onClick={handleRemoveFav}>REMOVE</Button>            
         </div>
+      
     )
 }
 
