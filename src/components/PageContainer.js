b830545a4ab322a0d68cd58ef883ccadd511bbe6
Janice Chan <janice.chan1288@gmail.com>
2022-05-23 13:56:44 -0400

    move state to App

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,28 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import Search from './Search'
 import ActivityList from './ActivityList'
-import Form from './Form'
-import FavoritesList from './FavoritesList'
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 
-const PageContainer = () => {
-const [activities, setActivities] = useState([])
-const [searchTerm, setSearchTerm] = useState("")
-const [display, setDisplay] = useState(false)
-const [trips, setTrips] = useState([])
-
-useEffect (() => {
-        fetch('http://localhost:4000/all')
-        .then(res => res.json())
-        .then(data => setActivities(data))
-      }, [])
-
-useEffect(() => {
-        fetch('http://localhost:4000/favorites')
-        .then(res => res.json())
-        .then(data => setTrips(data))
-    }, [])
+const PageContainer = ({activities, setActivities, onAddToFavs}) => {
+  const [searchTerm, setSearchTerm] = useState("")
 
 const handleSearchQuery = (searchQuery) => {
     setSearchTerm(searchQuery)
@@ -35,31 +18,16 @@ const handleDeleteCard = (id) => {
     const updatedActivities = activities.filter(act => act.id !== id)
     setActivities(updatedActivities)
 }    
-
-const handleAddActivity = (newActivity) => {
-    const updatedActivities = [...activities, newActivity]
-    setActivities(updatedActivities)
-}
-
-const AddToTrip = (activityObj) => {
-  const updatedTrips = [...trips, activityObj]
-  setTrips(updatedTrips)
-  console.log(updatedTrips) 
-}
-    
+  
     return (
       <div>
         <CssBaseline />
-          <Container maxWidth="xl">        
-            { display ? (<Form onHandleAddActivity={handleAddActivity} />) : null }
-            { display ? (<FavoritesList trips={trips} />) : null }
+          <Container maxWidth="xl">     
             <Search searchTerm={searchTerm} onHandleSearch={handleSearchQuery} />
-            <ActivityList activities={searchedActivities} onHandleDeleteCard={handleDeleteCard} onAddToTrip={AddToTrip} />
+            <ActivityList activities={searchedActivities} onHandleDeleteCard={handleDeleteCard} onAddToFavs={onAddToFavs} />
          </Container>
       </div>
-
     )   
 }
 
 export default PageContainer
-
